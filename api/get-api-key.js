bca0399e83b7d573d0436a3b7cc4040dc337fcf4
KikuchiNanako <a23.xgym@g.chuo-u.ac.jp>
2025-12-15 10:25:32 +0900

    fix: Add vercel.json rewrite rule for dynamic GPX3

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
+    console.log("Environment variable check:", process.env.GOOGLE_MAPS_API_KEY ? "Key Found": "Key NOT Found");
+    
     res.status(200).json({ key: process.env.GOOGLE_MAPS_API_KEY });
 }
